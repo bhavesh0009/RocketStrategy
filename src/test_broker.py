db35725d6d5ea1fedba12da2460ddedc82255638
karthick <prog@ecomsense.in>
2023-06-14 16:07:04 +0530

    remove credentials from test

--- src/test_broker.py
+++ src/test_broker.py
@@ -6,9 +6,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 sec_dir = "../../../"
 m = Fileutils().get_lst_fm_yml(sec_dir + "profitmart.yaml")
-profitmart = Finvasia(user_id=m['uid'], password=m['pwd'], pin=m['factor2'],
-                      vendor_code=m['vc'], app_key=m['app_key'], imei="1234",
-                      broker="profitmart")
-broker = profitmart.authenticate()
-token = profitmart.instrument_symbol("NSE", "SBIN-EQ")
-print(token)
+pmart = Finvasia(user_id=m['uid'], password=m['pwd'], pin=m['factor2'],
+                 vendor_code=m['vc'], app_key=m['app_key'], imei="1234",
+                 broker="profitmart")
+if pmart.authenticate():
+    print("success")
+
+token = pmart.instrument_symbol("NSE", "SBIN-EQ")
+resp = pmart.scriptinfo("NSE", token)
+print(resp)
--- test_api.py
+++ test_api.py
@@ -3,12 +3,6 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 api = NorenApiPy()
-uid = "35660003"
-pwd = "Ret2213@"
-factor2 = "AJLPD7024M"
-vc = "35660003"
-app_key = "pn39GKbQH3quTF8MQNQb395s4G6Dy8yN"
-imei = "1234"
 ret = api.login(userid=uid, password=pwd, twoFA=factor2,
                 vendor_code=vc, api_secret=app_key, imei=imei)
 print(ret)
